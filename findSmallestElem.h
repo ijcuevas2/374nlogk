f38ece52f4ecdf21e690775eeb465a17fd3c1352
Cuevas <icuevas2@sp16-cs241-120.cs.illinois.edu>
2016-03-06 10:54:52 -0600

    374

---
+++ findSmallestElem.h
@@ -0,0 +1,12 @@
+#ifndef _FINDSMALLESTELEM_H_
+#define _FINDSMALLESTELEM_H_
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+void findRanks(vector<int> A, vector<int> B, int k);
+
+#endif
