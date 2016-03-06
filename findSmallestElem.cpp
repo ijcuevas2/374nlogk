6b950eadbfc94d330a5c6061c3c61d12a1110c20
Cuevas <icuevas2@sp16-cs241-120.cs.illinois.edu>
2016-03-06 10:27:18 -0600

    374

--- findSmallestElem.cpp
+++ findSmallestElem.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <unistd.h>
 #include <stdio.h> 
+#include <assert.h>
 
 int recursiveTurns = 0;
 int main(){
@@ -12,35 +13,35 @@ int main(){
         A.push_back(i);
 
     random_shuffle(A.begin(), A.end());
-    A.resize(10); // Have an array with 10 random distinct numbers
+    int n = 10;
 
+    A.resize(n); // Have an array with n random distinct numbers
     vector<int> B;
-    B.push_back(1);
-    B.push_back(4);
-    B.push_back(6);
-    B.push_back(8);
+    
+   for(int i = 1; i < n; i++)
+        B.push_back(i);
 
-    //B.push_back(11);
-    //B.push_back(12);
-    //B.push_back(54);
+    //B.push_back(1);
+    //B.push_back(4);
+    //B.push_back(6);
+    //B.push_back(8);
+    
+    for(int i = 0; i < A.size(); i++)
+        printf("This is A[%d] (before findRanks): %d\n", i, A[i]);
+    for(int i = 0; i < B.size(); i++)
+        printf("This is B[%d]: %d\n", i, B[i]);
 
-    findQRanks(A, B);
+    findRanks(A, B, 0);
     return 0;
 }
 
 void findRanks(vector<int> A, vector<int> B, int k){ 
-    //printf("This is B.size() at start: %lu\n", B.size());
-    if(B.size() == 0)
-    {
-        printf("why\n"); //never supposed to happen 
-        return;
-    }
-    
+    assert(B.size());
+
     if(B.size() == 1)
     {
-        nth_element(A.begin(), A.begin() + B[0], A.end());  //pass in a subset vector 
-        printf("This is the level: %d\n", k);
-        printf("This is a leaf: %d\n", A[B[0]]);
+        nth_element(A.begin(), A.begin() + B[0]-1, A.end()-1);  
+        printf("This is the level: %d, kth smallest element: %d, leaf value: %d\n", k, B[0], A[B[0]-1]);
         return;
     }
 
@@ -50,43 +51,29 @@ void findRanks(vector<int> A, vector<int> B, int k){
     vector<int>::const_iterator start_2 = B.begin() + ((B.size())/2);
     vector<int>::const_iterator end_2;
 
-    //cout << "This is *start_1: " << *start_1 << endl;
-    //cout << "This is *end_1: " << *end_1 << endl;
-
-
     if(B.size()%2)
-    {
-        //printf("Entered B.size()mod2\n");
         end_2 = B.end() - 2;
-    }
 
     else
-    {
         end_2 = B.end() - 1;
-    }
 
-    //cout << "This is *start_2: " << *start_2 << endl;
-    //cout << "This is *end_2: " << *end_2 << endl;
-        
-    vector<int> leftSubArray(start_1,  end_1+1, k+1); //+1: To make the end iter NULL
-    vector<int> rightSubArray(start_2, end_2+1, k+1); //+1: To make the end iter NULL
+    //printf("This is *end_1+1: %d\n", *end_1+1);
+    vector<int> leftSubArray(start_1,  end_1+1); //+1: To make the end iter NULL
+    vector<int> rightSubArray(start_2, end_2+1); //+1: To make the end iter NULL
 
-    //for(auto & currVal : leftSubArray)
-    //    cout << "This is currVal: " << currVal << endl;
+    //printf("This is leftSubArray.size(): %lu\n", leftSubArray.size());
+    //printf("This is rightSubArray.size(): %lu\n", rightSubArray.size());
 
-    //cout << "This is leftSubArray.size(): " << leftSubArray.size() << endl;
-    //cout << "This is rightSubArray.size(): " << rightSubArray.size() << endl;
+    //for(auto & item: leftSubArray)
+    //    cout << "This is item: " << item << endl;
 
-    //recursiveTurns++;
-    //printf("This is recursiveTurns: %d\n", recursiveTurns);
-
-    findQRanks(A, leftSubArray);
-    findQRanks(A, rightSubArray);
+    findRanks(A, leftSubArray, k+1);
+    findRanks(A, rightSubArray, k+1);
 
     if(B.size()%2)
     {
         vector<int> sub_B_Array(1);
         sub_B_Array[0] = B[B.size()-1];
-        findQRanks(A, sub_B_Array); 
+        findRanks(A, sub_B_Array, k+1); 
     }
 }
